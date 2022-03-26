d634a45d7b979f572566a9e8812805604b677137
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-26 15:17:47 -0300

    Contact Page

---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Menu from "../../../src/components/menu/Menu";
+import {
+  faGithubSquare,
+  faLinkedin,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+function Contact() {
+  return (
+    <>
+      <Menu />
+      <div className="contact-content ">
+        <div className="content-icons">
+          <div style={{ fontSize: "1.5em" }}>
+            <a href="https://www.linkedin.com/in/damian-perez-">Linkedin</a>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </div>
+          <div className="a" style={{ fontSize: "1.5em" }}>
+            <a href="https://github.com/ReactGroup/reward-store-Damian/projects/">Github</a>
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </div>
+          <div className="a" style={{ fontSize: "1.5em" }}>
+            <a href="https://t.me/DPerez85">Telegram</a>
+            <FontAwesomeIcon icon={faTelegram} />
+          </div>
+          <div className="a" style={{ fontSize: "1.5em" }}>
+            <a href="mailto:damianperez22@hotmail.com">Email</a>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+        </div>
+
+        <div className="contact-right">
+          <p className="text-paragraph">
+            Mi nombre es Damián Pérez, soy estudiante de Sistemas y estoy dando
+            mis primeros pasos profesionales en el mundo IT. <br /> Intento
+            especializarme en desarrollo Web, por cierto mi gran pasión.
+          </p>
+          <p className="text-paragraph">
+            Gracias a este proyecto y también a la incorporacion de otras
+            herramientas adquiridas en mi área de estudio siento estar preparado
+            para dar este paso e insertarme en este nuevo mundo.
+          </p>
+          <p className="text-paragraph">
+            Espero que tu experiencia dentro de este sitio sea agradable, y
+            cualquier sugerencia que aparezca es totalmente recibida.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Contact;
--- src/pages/principal/Principal.jsx
+++ src/pages/principal/Principal.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../home/home";
 import NotFound from "../404/notFound"; 
+import Contact from "../contact/contact";
 
 
 function Principal() {
@@ -11,6 +12,7 @@ function Principal() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/home" element={<Home/>} />
+          <Route path="/contact" element={<Contact/>} />
           <Route path="/*" element={<NotFound/>} />
 
          </Routes>
