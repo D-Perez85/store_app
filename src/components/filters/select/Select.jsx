8bea356c4a169cb6dd5f60961409b7b75e0ffb33
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-26 17:42:47 -0300

    Filter by Price & Article

--- src/components/filters/select/Select.jsx
+++ src/components/filters/select/Select.jsx
@@ -1,12 +1,44 @@
-import React from "react";
- 
-function Select() {
+import React, { useContext } from "react";
+import { ProductContext } from "../../../context/productContext";
+
+function Select({ filter }) {
+  const { items, copiaItems } = useContext(ProductContext);
+  let repeat = {};
+  let result = items.filter(function (value) {
+    let exists = !repeat[value.category];
+    repeat[value.category] = true;
+    return exists;
+  });
+
+  function filterByArticle(e) {
+    let filterUpdate = items.filter((value) => {
+      return e.target.value === "All Categories"
+        ? value
+        : value.category === e.target.value;
+    });
+    filter(filterUpdate);
+  }
+  function filterHigher(e) {
+    let array = [...copiaItems];
+    let order = array.sort((a, b) => b.cost - a.cost);
+    filter(order);
+  }
+
+  function filterLower(e) {
+    let array = [...copiaItems];
+    let order = array.sort((a, b) => a.cost - b.cost);
+    filter(order);
+  }
+
   return (
     <>
-      <select name="categories" id="">
+      <select onChange={filterByArticle} name="categories" id="">
         <option className="option" value="All Categories">
           All Categories
         </option>
+        {result.map((value, index) => {
+          return <option key={index}>{value.category}</option>;
+        })}
       </select>
 
       <input
@@ -15,8 +47,11 @@ function Select() {
         id="lowest"
         name="filter"
         value="lowest"
-       />
-      <label id="order-label" for="lowest">Lower Price</label>
+        onClick={filterLower}
+      />
+      <label id="order-label" for="lowest">
+        Lower Price
+      </label>
 
       <input
         className="input-filter"
@@ -24,8 +59,11 @@ function Select() {
         id="highest"
         name="filter"
         value="highest"
-       />
-      <label id="order-label"  for="highest">Higher Price</label>
+        onClick={filterHigher}
+      />
+      <label id="order-label" for="highest">
+        Higher Price
+      </label>
     </>
   );
 }
